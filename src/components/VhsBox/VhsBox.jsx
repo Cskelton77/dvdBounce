fa2c31b8e4207567cff96a1bb654543c60ca8ea4
Cain Skelton <cain@Cains-MacBook-Air.local>
2023-09-15 17:39:32 +0100

    Deploy build

--- src/components/VhsBox/VhsBox.jsx
+++
@@ -1,9 +0,0 @@
-// Image by <a href="https://www.freepik.com/free-vector/flat-design-vhs-cover-template_33597779.htm#query=retro%20vhs&position=9&from_view=keyword&track=ais">Freepik</a>
-import vhs from "./vhs.jpg";
-import styles from "./VhsBox.module.scss";
-
-const VhsBox = ({ style }) => {
-  return <img src={vhs} className={styles.vhsBox} style={style} alt="" />;
-};
-
-export default VhsBox;
